e1e886fc032e8d2b5b45876c892f8e74369ea9f5
PreethiKokila <kokilaz@gmail.com>
2020-09-15 22:51:16 -0400

    first commit

--- cmp-databinding/src/app/app.component.ts
+++ cmp-databinding/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cmp-databinding';
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Testing my server!'}];
 }
